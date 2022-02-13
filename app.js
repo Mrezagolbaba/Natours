f0ccfcacd7bcdbfd51b4172e0548307868e11047
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-02-13 22:21:07 +0330

    create get API tours

---
+++ app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+const port = 7000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
