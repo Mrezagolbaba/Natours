732ead64c650e982fdc5e381bc2f42e2343333a0
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-02-15 11:15:07 +0330

    create post request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -15,6 +16,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(
+    {
+      id: newId,
+    },
+    req.body
+  );
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 7000;
