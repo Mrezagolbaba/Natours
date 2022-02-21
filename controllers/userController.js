c03c7f53c89ac4d7d311f86421558c7418381ed1
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-02-21 09:39:20 +0330

    create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not defined',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not defined',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not defined',
+  });
+};
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not defined',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not defined',
+  });
+};
