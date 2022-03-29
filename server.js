4cb03a49ea8b4db488ae2d183c167b674d87d615
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-03-29 23:45:32 +0430

    install mongoos and config server file

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
 // Start Server
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${port}`);
 });
