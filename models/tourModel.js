afadeec905df9129b27a91eef980c7ae817a1ea9
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-04-07 00:44:15 +0430

    refactor tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,51 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'], // we send this error message if the name is not provided
     unique: true, // we dont have two tours with the same name
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+    trim: true,
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5, // default value if no rating is provided
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imgCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
