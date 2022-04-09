01db55a22b4d70479c972e18cdc9404dc6b1dc7f
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-04-09 09:18:39 +0430

    update error message

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -48,7 +48,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
