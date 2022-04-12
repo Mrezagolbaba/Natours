e0375b94155fd37fd0c42902699607011e9ba723
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-04-12 22:30:58 +0430

    add pagination feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,6 +31,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // D) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     // const query = Tour.find() //mongoos find() method
     //   .where('duration')
     //   .equals(5)
