77b549b733e3cd26dd1420e13aadd46cfe6831d5
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-04-12 22:01:39 +0430

    add sort feature to api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,7 +14,14 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr)); // findById for finding all documents
+    let query = Tour.find(JSON.parse(queryStr)); // findById for finding all documents
+    // B) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // const query = Tour.find() //mongoos find() method
     //   .where('duration')
