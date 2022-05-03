2922fe426cd4f9107494ba1d4a111b999cca62b0
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-05-04 00:06:07 +0430

    create slug generator

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'], // we send this error message if the name is not provided
       unique: true, // we dont have two tours with the same name
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -62,6 +64,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDLWARE: runs before .save and . create
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {
+    lower: true,
+  });
+  next();
+});
+// tourSchema.pre('save', (next) => {
+//   console.log('will save document ');
+//   next();
+// });
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
