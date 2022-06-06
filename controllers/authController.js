29fe2e55085584f2d4ed8750653bf3c9cc1cad6b
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-06-06 09:03:04 +0430

    create login API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const CatchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -39,4 +40,23 @@ exports.signup = CatchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
   createSendToken(newUser, 201, res);
+});
+exports.login = CatchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
 });
