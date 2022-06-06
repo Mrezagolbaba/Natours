d8d4dc189904e2572ed19694e5927841d1867dfa
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-06-06 09:02:50 +0430

    create get all tour api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
+const User = require('../models/userModel');
+const CatchAsync = require('../utils/catchAsync');
+
 const express = require('express');
 
 exports.getUser = (req, res) => {
@@ -18,12 +21,19 @@ exports.deleteUser = (req, res) => {
     message: 'Route not defined',
   });
 };
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Route not defined',
+exports.getAllUsers = CatchAsync(async (req, res, next) => {
+  // execute query
+  const users = await User.find();
+
+  // send response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
